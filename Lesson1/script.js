313f971c1e9afd05bf1caa52e25354486073708b
Leonid <85498423+Notforclmbng@users.noreply.github.com>
2021-07-13 16:50:45 +0300

    Add files via upload

---
+++ Lesson1/script.js
@@ -0,0 +1,152 @@
+// var a = 1;
+// const b = 123;
+// let b = 123;
+// b = 5;
+// a = 4;
+// console.log(a);
+// console.log(b);
+
+// let userName = 'Vasya'; // camelCase
+
+// {
+//     var a = 1;
+//     let b = 2;
+//
+//     console.log(a);
+//     console.log(b);
+// }
+
+// let a = 123;
+//
+// {
+//     a = 2344;
+//
+//     {
+//         let a = 333;
+//         console.log(a);
+//     }
+// }
+//
+// console.log(a)
+
+// function foo() {
+//     var a = 123;
+// }
+// foo();
+//
+// console.log(a);
+// console.log(b);
+
+// var name = 'Vasya';
+// name = 1;
+//
+// console.log(name);
+
+// let a = 'Привет';
+// let a = "Привет";
+// let a = `Привет`;
+// console.log(a);
+
+// let user = 'Petya';
+// let string = 'Привет ' + user + '!';
+// let string = `Привет ${user}!`;
+//
+// console.log(string);
+
+// console.log('первая строка\nвторая строка');
+// console.log(`первая строка
+// вторая строка`);
+// console.log('первая строка\
+//  вторая строка');
+
+// console.log('Он сказал - "Привет!".');
+// console.log('Он сказал - \'Привет!\'.');
+// console.log("Он сказал - 'Привет!'.");
+// console.log("Он сказал - \"Привет!\".");
+// console.log(`Он сказал\` - 'Привет!".`);
+
+// let a = 1;
+// let b = 3.14;
+// let c = 10 / 0;
+
+// console.log(c);
+// console.log(typeof c);
+// console.log(typeof b);
+// Math
+
+// console.log(10 * 'Hi!'); // => 10 * NaN => NaN;
+// console.log(typeof NaN);
+
+// let i = '3.14';
+
+// console.log(typeof i, i);
+// i = +i;
+// i = -i;
+// console.log(typeof i, i);
+// console.log(parseInt(i));
+// console.log(parseFloat(i));
+// console.log(Number(i));
+
+// console.log(0xf);
+// console.log(2e3);
+// console.log(2e-3);
+
+// boolean (true / false)
+// null
+// undefined
+// Object
+// symbol
+
+// function foo() {
+//
+// }
+
+// console.log(typeof foo);
+// console.log(typeof []);
+// console.log([] instanceof Array);
+// console.log(typeof null);
+
+// console.log(6 + 4);
+// console.log(6 - 4);
+// console.log(6 * 4);
+// console.log(6 / 4);
+// console.log(6 ** 4);
+// console.log(6 % 4);
+
+// let a = 1;
+// a += 2; // a = a + 2;
+//
+// console.log(a);
+
+// console.log('первая строка' + ' вторая строка');
+// console.log('1' + '1'); // 11
+// console.log('1' + 1); // 11
+// console.log(1 + '1'); // 11
+// console.log(1 + 1);
+
+
+// const a = 5; b = 5;
+// a > b;
+// a >= b;
+// a < b;
+// a <= b;
+// a == b;
+// a === b;
+// a != b;
+// a !== b;
+
+
+/*
+* */
+
+// /**
+//  * Some description
+//  * @param a {number}
+//  * @param b {number}
+//  * @returns {number}
+//  */
+// function sum(a, b) {
+//     return a + b;
+// }
+//
+// console.log(sum(5, 6));
